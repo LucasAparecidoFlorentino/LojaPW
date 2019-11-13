9c7986bfa1c0667289fb6b4015ee22048881b625
LucasAparecidoFlorentino <37313637+LucasAparecidoFlorentino@users.noreply.github.com>
2019-11-13 01:16:07 -0200

    Continuar a partir da aula 9

---
+++ loja/src/main/java/com/dev/loja/controller/FuncionarioController.java
@@ -0,0 +1,43 @@
+package com.dev.loja.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.dev.loja.models.Funcionario;
+import com.dev.loja.repository.FuncionarioRepository;
+
+@Controller
+public class FuncionarioController {
+	
+	@Autowired
+	private FuncionarioRepository funcionarioRepository;
+	
+	@GetMapping("/administrativo/funcionarios/cadastrar")
+	public ModelAndView cadastrar(Funcionario funcionario) {
+		ModelAndView mv = new ModelAndView("administrativo/funcionarios/cadastro");
+		mv.addObject("funcionario", funcionario);
+		return mv;
+	}
+	
+	@GetMapping("/administrativo/funcionarios/listar")
+	public String listar() {
+		return "administrativo/funcionarios/lista";
+	}
+	
+	@PostMapping("/administrativo/funcionarios/salvar")
+	public ModelAndView salvar(@Valid Funcionario funcionario, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			return cadastrar(funcionario);
+		}
+		funcionarioRepository.saveAndFlush(funcionario);
+		return cadastrar(new Funcionario());
+	}
+
+}
