f2d4b08812050698b0603d5aa2085e86412ca7e4
Aluno <Aluno@INFOLAB03MC25>
2019-11-26 18:52:01 -0200

    Projeto pronto

--- loja/src/main/java/com/dev/loja/controller/CarrinhoController.java
+++ loja/src/main/java/com/dev/loja/controller/CarrinhoController.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.dev.loja.models.Cliente;
@@ -18,47 +19,55 @@ import com.dev.loja.models.Compra;
 import com.dev.loja.models.ItensCompra;
 import com.dev.loja.models.Produto;
 import com.dev.loja.repository.ClienteRepository;
+import com.dev.loja.repository.CompraRepository;
+import com.dev.loja.repository.ItensCompraRepository;
 import com.dev.loja.repository.ProdutoRepository;
 
 @Controller
 public class CarrinhoController {
-	
+
 	private List<ItensCompra> itensCompra = new ArrayList<ItensCompra>();
 	private Compra compra = new Compra();
 	private Cliente cliente;
-	
+
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
+
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
+
+	@Autowired
+	private CompraRepository compraRepository;
+
+	@Autowired
+	private ItensCompraRepository itensCompraRepository;
+
 	private void calcularTotal() {
 		compra.setValorTotal(0.);
-		for(ItensCompra it : itensCompra) {
+		for (ItensCompra it : itensCompra) {
 			compra.setValorTotal(compra.getValorTotal() + it.getValorTotal());
 		}
 	}
 
 	@GetMapping("/carrinho")
-	public ModelAndView chamarCarrinho()  {
+	public ModelAndView chamarCarrinho() {
 		ModelAndView mv = new ModelAndView("cliente/carrinho");
 		calcularTotal();
 		mv.addObject("compra", compra);
 		mv.addObject("listaItens", itensCompra);
 		return mv;
 	}
-	
+
 	private void buscarUsuarioLogado() {
 		Authentication autenticado = SecurityContextHolder.getContext().getAuthentication();
-		if(!(autenticado instanceof AnonymousAuthenticationToken)) {
+		if (!(autenticado instanceof AnonymousAuthenticationToken)) {
 			String email = autenticado.getName();
 			cliente = clienteRepository.buscarClienteEmail(email).get(0);
 		}
 	}
-	
+
 	@GetMapping("/finalizar")
-	public ModelAndView finalizarCompra()  {
+	public ModelAndView finalizarCompra() {
 		buscarUsuarioLogado();
 		ModelAndView mv = new ModelAndView("cliente/finalizar");
 		calcularTotal();
@@ -67,52 +76,68 @@ public class CarrinhoController {
 		mv.addObject("cliente", cliente);
 		return mv;
 	}
-	
+
+	@PostMapping("/finalizar/confirmar")
+	public ModelAndView confirmarCompra(String formaPagamento) {
+		ModelAndView mv = new ModelAndView("cliente/mensagemFinalizou");
+		compra.setCliente(cliente);
+		compra.setFormaPagamento(formaPagamento);
+		compraRepository.saveAndFlush(compra);
+
+		for (ItensCompra c : itensCompra) {
+			c.setCompra(compra);
+			itensCompraRepository.saveAndFlush(c);
+		}
+		itensCompra = new ArrayList<>();
+		compra = new Compra();
+		return mv;
+	}
+
 	@GetMapping("/alterarQuantidade/{id}/{acao}")
-	public String alterarQuantidade(@PathVariable Long id, @PathVariable Integer acao)  {
-		
-		for(ItensCompra it : itensCompra) {
+	public String alterarQuantidade(@PathVariable Long id, @PathVariable Integer acao) {
+
+		for (ItensCompra it : itensCompra) {
 			if (it.getProduto().getId().equals(id)) {
-				if(acao.equals(1)) {
-				it.setQuantidade(it.getQuantidade() + 1);
-				it.setValorTotal(0.);
-				it.setValorTotal(it.getValorTotal()+ (it.getQuantidade()*it.getValorUnitario()));
-				}else if(acao == 0) {
+				if (acao.equals(1)) {
+					it.setQuantidade(it.getQuantidade() + 1);
+					it.setValorTotal(0.);
+					it.setValorTotal(it.getValorTotal() + (it.getQuantidade() * it.getValorUnitario()));
+				} else if (acao == 0) {
 					it.setQuantidade(it.getQuantidade() - 1);
 					it.setValorTotal(0.);
-					it.setValorTotal(it.getValorTotal()+ (it.getQuantidade()*it.getValorUnitario()));
+					it.setValorTotal(it.getValorTotal() + (it.getQuantidade() * it.getValorUnitario()));
 				}
 				break;
 			}
-		}	
-		
+		}
+
 		return "redirect:/carrinho";
 	}
-	
+
 	@GetMapping("/removerProduto/{id}")
-	public String removerProdutoCarrinho(@PathVariable Long id)  {
-		
-		for(ItensCompra it : itensCompra) {
+	public String removerProdutoCarrinho(@PathVariable Long id) {
+
+		for (ItensCompra it : itensCompra) {
 			if (it.getProduto().getId().equals(id)) {
 				itensCompra.remove(it);
 				break;
 			}
-		}	
-		
+		}
+
 		return "redirect:/carrinho";
 	}
-	
+
 	@GetMapping("/adicionarCarrinho/{id}")
-	public String adicionarCarrinho(@PathVariable Long id)  {
+	public String adicionarCarrinho(@PathVariable Long id) {
 		Optional<Produto> prod = produtoRepository.findById(id);
 		Produto produto = prod.get();
-		
+
 		int controle = 0;
-		for(ItensCompra it : itensCompra) {
+		for (ItensCompra it : itensCompra) {
 			if (it.getProduto().getId().equals(produto.getId())) {
 				it.setQuantidade(it.getQuantidade() + 1);
 				it.setValorTotal(0.);
-				it.setValorTotal(it.getValorTotal() + (it.getQuantidade()*it.getValorUnitario()));
+				it.setValorTotal(it.getValorTotal() + (it.getQuantidade() * it.getValorUnitario()));
 				controle = 1;
 				break;
 			}
@@ -121,8 +146,8 @@ public class CarrinhoController {
 			ItensCompra item = new ItensCompra();
 			item.setProduto(produto);
 			item.setValorUnitario(produto.getValorVenda());
-			item.setQuantidade(item.getQuantidade()+1);
-			item.setValorTotal(item.getValorTotal()+ (item.getQuantidade()*item.getValorUnitario()));
+			item.setQuantidade(item.getQuantidade() + 1);
+			item.setValorTotal(item.getValorTotal() + (item.getQuantidade() * item.getValorUnitario()));
 			itensCompra.add(item);
 		}
 		return "redirect:/carrinho";
